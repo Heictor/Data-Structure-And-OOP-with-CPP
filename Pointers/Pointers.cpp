ed00ea188c5d9507902040233932c5e13d551e0f
Heictor <39010251+heictor@users.noreply.github.com>
2019-05-23 22:30:32 -0300

    Commit #1

---
+++ Pointers/Pointers.cpp
@@ -0,0 +1,27 @@
+//
+// Created by heict on 23/05/2019.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void foo(int* n){
+    *n = 15;
+}
+int main(){
+    //Declare both variable and pointer
+    int var = 10;
+    int* pvar;
+
+    //Initialize pointer
+    pvar = &var;
+    var = 20;
+    //The pointer pvar will get var's memory address
+    cout<<pvar<<endl<<*pvar<<endl;
+    cout<<var<<endl<<&var<<endl;
+
+    foo(pvar);
+    cout<<*pvar<<endl<<var<<endl;
+    return 0;
+}
