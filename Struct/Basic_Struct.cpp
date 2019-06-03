e87bef51c0e572768faa5d97343e3c89e7d68b19
Heictor <heictor8@gmail.com>
2019-06-03 19:00:02 -0300

    Using vector on scruct elements

--- Struct/Basic_Struct.cpp
+++ Struct/Basic_Struct.cpp
@@ -12,16 +12,33 @@ typedef struct profile{
 }prof;
 
 int main(){
-    prof p[3];
+    prof p[3]; //Instantiate 3 objects from the struct
+    prof pon; //Instantiate a object
+    prof* ppon; //Instantiate a struct pointer
+
+    ppon = &pon;
 
     strcpy(p[0].name, "Heictor");
     p[0].age = 19;
+
     strcpy(p[1].name, "Brennus");
-    p[1].age = 20;    
+    p[1].age = 20;
+
+    strcpy(ppon->name, "Julio");
+    ppon->age = 18;
 
     cout<<"Name: "<<p[0].name<<endl;
     cout<<"Age: "<<p[0].age<<endl;
+
     cout<<"Name: "<<p[1].name<<endl;
     cout<<"Age: "<<p[1].age<<endl;
+
+    //Common way
+    cout<<"Name: "<<pon.name<<endl;
+    cout<<"Age: "<<pon.age<<endl;
+    //Pointer way
+    cout<<"Name: "<<ppon->name<<endl;
+    cout<<"Age: "<<ppon->age<<endl;
+
     return 0;
 }
